3ed2229a27824fa358a77115dbbb621ee75dd31e
Gabriel Balbuena <agbalbuenaluna@gmail.com>
2025-09-18 23:46:51 -0600

    [NAVIGATION-COMPONENT-FIRST-CONFIGURATION] This commit contains the logic needed in order to add the navigation component logic
    -Enum Class Action has several actions
    -Navigation Dependencies
    - CONSTANTS Utils
    - Toml version catalog Navigation and HILT
    -LIST SCREEN Composable
    -TASK SCREEN Composable
    
    Navigation class has a fun that receives a navController NavHostController then uses remember in order to remember the stack of views, and contain a NavHost that use a NavController and a destination
    and inside the body declare the logic to ComposableScreens
    
    (we are using Screens class in order to navigate, and MainActivity contains NavHostController)

---
+++ app/src/main/java/com/example/todoapp/navigation/Navigation.kt
@@ -0,0 +1,33 @@
+package com.example.todoapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.example.todoapp.navigation.destinations.listComposable
+import com.example.todoapp.navigation.destinations.taskComposable
+import com.example.todoapp.util.Constants.LIST_SCREEN
+
+
+@Composable
+fun SetupNavigation(
+    navController: NavHostController
+) {
+    val screen = remember(navController) {
+        Screens(navController = navController)
+    }
+
+    NavHost(
+        navController = navController,
+        startDestination = LIST_SCREEN
+    ) {
+        //NavGraphBuilder este es el builder de navegación
+        listComposable(
+            navigateToTaskScreen = screen.task
+        )
+
+        taskComposable(
+            navigateToListScreen = screen.list
+        )
+    }
+}
